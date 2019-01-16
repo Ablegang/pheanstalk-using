d8a03e1e3364c651a58eeac45003399fd06b38d1
object <2252390865@qq.com>
2019-01-16 10:48:35 +0800

    consumer

--- consumer.php
+++ consumer.php
@@ -16,9 +16,9 @@ $conn = \Pheanstalk\Pheanstalk::create('beanstalkd');
 //$job = $conn->reserve(); // 默认情况会接收 default tube 的 job
 //print_r($job); // 如果 reserve 没能得到 job ，就会一直阻塞在上面
 
-$conn->watch('test')->watch('big');
-$conn->ignore('default');
-$conn->watchOnly('order');
+//$conn->watch('test')->watch('big');
+//$conn->ignore('default');
+//$conn->watchOnly('order');
 
 try {
     $job2 = $conn->reserveWithTimeout(10);  // 阻塞接收，10秒之后超时，就不再接收了
